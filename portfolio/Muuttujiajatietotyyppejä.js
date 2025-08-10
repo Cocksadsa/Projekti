4cd0e737f978fdf5b20a71400ed29aebfb80c782
Viineri <142116498+Cocksadsa@users.noreply.github.com>
2025-08-11 01:44:19 +0300

    Add files via upload

---
+++ portfolio/Muuttujiajatietotyyppejä.js
@@ -0,0 +1,24 @@
+console.log("Muuttujia ja tietotyyppejä avattu!")
+let a = parseFloat(document.getElementById("1").value) || 0;
+let b = parseFloat(document.getElementById("2").value) || 0;
+
+function laskin_summa() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+let b = parseFloat(document.getElementById("2").value) || 0;
+    alert(a + b);
+}
+function laskin_vähennys() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+let b = parseFloat(document.getElementById("2").value) || 0;
+    alert(a - b);
+}
+function laskin_kerto() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+let b = parseFloat(document.getElementById("2").value) || 0;
+    alert(a * b);
+}
+function laskin_jako() {
+    let a = parseFloat(document.getElementById("1").value) || 0;
+let b = parseFloat(document.getElementById("2").value) || 0;
+    alert(a / b);
+}
--- portfolio/keskustelusivu.js
+++ portfolio/keskustelusivu.js
@@ -1,1 +1,19 @@
 console.log("keskustelusivu avattu");
+document.getElementById("avaaDialogi").addEventListener("click", function() {
+    document.getElementById("dialogini").showModal();
+    });
+function send(event) {
+event.preventDefault();
+let birthdayField = document.getElementById("birthday");
+let emailField = document.getElementById("email");
+let typeField = document.getElementById("type")
+let usageField = document.getElementById("usage");
+let bodyField = document.getElementById("body")
+let birthday = birthdayField.value;
+let email = emailField.value;
+let type = typeField.value;
+let usage = usageField.value;
+let body = bodyField.value;
+console.log(birthday, email, type, usage, body)
+document.getElementById("dialogini").close();
+} 
